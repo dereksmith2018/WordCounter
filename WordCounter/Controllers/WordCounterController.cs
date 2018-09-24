f8f50b47b5031351ea07fa5ebe8353c08e6311e0
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2018-09-23 19:43:24 -0700

    added contoller and fixed paths

---
+++ WordCounter/Controllers/WordCounterController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+using System.Collections.Generic;
+
+namespace WordCounter.Controllers
+{
+    public class WordCounterController : Controller
+    {
+        [HttpGet("/WordCounter/CreateSentence")]
+        public ActionResult CreateSentence()
+        {
+            return View();
+        }
+        [HttpPost("/WordCounter/CreateWord")]
+        public ActionResult CreateWord()
+        {
+            RepeatCounter newCounter = new RepeatCounter();
+            newCounter.SetUserSentence(Request.Form["new-sentence"]);
+            return View(newCounter);
+
+        }
+        [HttpPost("/WordCounter/Counter")]
+        public ActionResult CreateCount()
+        {
+            RepeatCounter newCounter = new RepeatCounter();
+            newCounter.SetUserSentence(Request.Form["entered-sentence"]);
+            newCounter.SetUserWord(Request.Form["new-word"]);
+            return View("Counter", newCounter);
+        }
+        // [HttpPost("/words")]
+        // public ActionResult CreateCount()
+        // {
+        //     RepeatCounter userInput = new RepeatCounter(Request.Form["new-word"], Request.Form["new-sentence"]);
+        //     userInput.CountWord();
+        //     userInput.Save();
+        //     List<RepeatCounter> allItems = RepeatCounter.GetAll();
+        //     return View("Index", allItems);
+        // }
+    }
+
+}
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -1,25 +1,32 @@
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 
-namespace WordCounter
+namespace WordCounter.Models
 {
      public class RepeatCounter
     {
     private string _userSentence;
     private string _userWord;
+    private int _count = 0;
+    // private static List<RepeatCounter> _allCounters = new List<RepeatCounter> {};
 
+    // public RepeatCounter(string userWord, string userSentence)
+    // {
+    //   _userSentence = userSentence;
+    //   _userWord = userWord;
+    // }
 
-    public void SetUserSentence(string newUserSentence)
+    public void SetUserSentence(string userSentence)
     {
-      _userSentence = newUserSentence;
+      _userSentence = userSentence;
     }
     public string GetUserSentence()
     {
       return _userSentence;
     }
-    public void SetUserWord(string newUserWord)
+    public void SetUserWord(string userWord)
     {
-      _userWord = newUserWord;
+      _userWord = userWord;
     }
     public string GetUserWord()
     {
@@ -30,19 +37,33 @@ namespace WordCounter
        string[] sentence = _userSentence.Split(' ');
        return sentence;
     }
-
-    public int CountWord()
+    // public static List<RepeatCounter> GetAll()
+    // {
+    //   return _allCounters;
+    // }
+    // public void Save()
+    // {
+    //   _allCounters.Add(this);
+    // }
+    public int CountedWord()
     {
-
-    string[] sentence = this.MakeSentenceArray();
-    int count = 0;
-    foreach(string word in sentence)
-      count = (word.Equals(_userWord)) ? 1 : 0;
-          {
-            return count;
-          }
-
+      _count = 0;
+      string[] newSentence = MakeSentenceArray();
+      for (int i = 0; i < newSentence.Length; i++)
+      {
+        _count +=(_userWord == newSentence[i]) ? 1 : 0;
+      }
+      return _count;
     }
+    //   _count = 0;
+    // string[] sentence = this.MakeSentenceArray();
+    // foreach(string word in sentence)
+    //   count = (word.Equals(_userWord)) ? 1 : 0;
+    //       {
+    //         return count;
+    //       }
+    //
+    // }
   }
 }
 /* string[] sentence = usersentence.Split(" ");
